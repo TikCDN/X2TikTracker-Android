59cb8bc20fdae9995b3bc83fa82d134bf0618d97
淘二二 <928073632@qq.com>
2024-12-27 10:33:44 +0800

    update sdk

--- Prj-Android/app/src/main/java/org/anyrtc/tiktracker/MainActivity.kt
+++ Prj-Android/app/src/main/java/org/anyrtc/tiktracker/MainActivity.kt
@@ -1,8 +1,16 @@
 package org.anyrtc.tiktracker
 
+import android.content.Context
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
+import android.content.pm.Signature
+import android.graphics.drawable.Drawable
+import android.icu.lang.UCharacter.GraphemeClusterBreak.L
 import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.media3.common.MediaItem
 import androidx.media3.datasource.DataSource
 import androidx.media3.datasource.DefaultHttpDataSource
@@ -10,12 +18,23 @@ import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.exoplayer.hls.HlsMediaSource
 import io.anyrtc.x2tiktracker.*
 import org.anyrtc.tiktracker.databinding.ActivityMainBinding
+import java.security.MessageDigest
+import java.security.cert.CertificateFactory
+import java.security.cert.X509Certificate
+
+
+import java.security.interfaces.RSAPublicKey
+import java.util.HexFormat
+
+
 
 class MainActivity : AppCompatActivity() {
 
     private val x2HsrEngine by lazy { X2TikTrackerEngine(this,Config.appId) }
 
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,16 +46,11 @@ class MainActivity : AppCompatActivity() {
 
 
                 override fun onShareResult(code: TKT_CODE?) {
+                    setTitle(code?.name)
                 }
 
                 override fun onLoadDataStats(stats: DataStats) {
                     binding.m = stats
-                }
-
-                override fun onRenewTokenResult(token: String, errorCode: RenewTokenErrCode?) {
-                }
-
-                override fun onTokenWillExpire() {
                 }
 
                 override fun onPeerOff(peerId: String, peerData: String) {
@@ -45,8 +59,6 @@ class MainActivity : AppCompatActivity() {
                 override fun onPeerOn(peerId: String, peerData: String) {
                 }
 
-                override fun onTokenExpired() {
-                }
 
             })
             x2HsrEngine.startPlay(playerUrl,true)
